f0c1ca3ebaf6ce6fca6189dbbd19efb804ab66b0
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2020-01-10 11:19:30 -0800

    Initial Commit

---
+++ SliderDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderDemo
+//
+//  Created by Ahmed M. Hassan on 1/10/20.
+//  Copyright © 2020 Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
